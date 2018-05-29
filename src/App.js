8a2968a391c6e4f0ec50ebe72c24a3d6a9f36c4c
Anne <annehwatson@gmail.com>
2018-05-29 14:27:48 -0700

    npm install

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Application title</h1>
+          <h1 className="App-title">Chat between remote and local</h1>
         </header>
         <main className="App-main">
         </main>
